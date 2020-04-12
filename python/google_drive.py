5ec4b4d88f34a3c551e5af8c1f331dcc909119ca
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2020-04-12 23:14:02 +0200

    style: black formatter

--- python/google_drive.py
+++ python/google_drive.py
@@ -15,16 +15,18 @@ class SpreadSheet:
     """
 
     def __init__(self):
-        scope = ['https://spreadsheets.google.com/feeds',
-                 'https://www.googleapis.com/auth/drive']
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive",
+        ]
         creds = ServiceAccountCredentials.from_json_keyfile_name(
-            'credentials.json', scope)
+            "credentials.json", scope
+        )
         client = gspread.authorize(creds)
-        self.sheet = client.open('worldometers').sheet1
+        self.sheet = client.open("worldometers").sheet1
 
     def append_row(
-        self,
-        data_dict: Dict[str, Any],
+        self, data_dict: Dict[str, Any],
     ):
         """Appends a row to the spreadsheet. This function uses the 
         following values:
@@ -43,41 +45,28 @@ class SpreadSheet:
         """
 
         values = [
-            data_dict['country'],
-            data_dict['active_cases'],
-            data_dict['cases_per_million'],
-            data_dict['critical'],
-            data_dict['new_cases'],
-            data_dict['new_deaths'],
-            data_dict['total_cases'],
-            data_dict['total_deaths'],
-            data_dict['total_recoveries'],
+            data_dict["country"],
+            data_dict["active_cases"],
+            data_dict["cases_per_million"],
+            data_dict["critical"],
+            data_dict["new_cases"],
+            data_dict["new_deaths"],
+            data_dict["total_cases"],
+            data_dict["total_deaths"],
+            data_dict["total_recoveries"],
         ]
 
-        self.sheet.append_row(
-            values=values,
-        )
+        self.sheet.append_row(values=values,)
 
-    def update_lang(
-        self,
-        index: int,
-        lang: str,
-    ) -> None:
+    def update_lang(self, index: int, lang: str,) -> None:
         "TODO: Mind explaining @vipermu ?"
         self.sheet.update_cell(
-            row=index + 2,
-            col=2,
-            value=lang,
+            row=index + 2, col=2, value=lang,
         )
 
-    def update_history(
-        self,
-        index: int,
-        new_hist: str,
-    ) -> None:
+    def update_history(self, index: int, new_hist: str,) -> None:
         "TODO: Mind explaining @vipermu ?"
         self.sheet.update_cell(
-            row=index + 2,
-            col=3,
-            value=new_hist,
+            row=index + 2, col=3, value=new_hist,
         )
+
--- python/scraper.py
+++ python/scraper.py
@@ -7,52 +7,54 @@ from bs4 import BeautifulSoup
 import google_drive as gd
 
 
-def get_soup_from_url(
-    url: str
-) -> BeautifulSoup:
+def get_soup_from_url(url: str) -> BeautifulSoup:
     resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'lxml')
+    soup = BeautifulSoup(resp.text, "lxml")
     return soup
 
 
 def process_data_item(data):
     "TODO: @vipermu can you input the return type?"
     data = data.text.strip()
-    data = data.replace(',', '')
-    data = data.replace('+', '')
-    if data == '':
-        data = '0'
+    data = data.replace(",", "")
+    data = data.replace("+", "")
+    if data == "":
+        data = "0"
     if data.isdecimal():
         data = int(data)
     return data
 
 
-def get_table_data_from_soup(
-    soup: BeautifulSoup,
-) -> dict:
+def get_table_data_from_soup(soup: BeautifulSoup,) -> dict:
     data_dict = {}
     key_list = [
-        'country', 'total_cases', 'new_cases', 'total_deaths',
-        'new_deaths', 'total_recoveries', 'active_cases', 'critical',
-        'cases_per_million',
+        "country",
+        "total_cases",
+        "new_cases",
+        "total_deaths",
+        "new_deaths",
+        "total_recoveries",
+        "active_cases",
+        "critical",
+        "cases_per_million",
     ]
-    table = soup.find('table', {'id': 'main_table_countries_today'})
-    row_list = table.find_all('tr')
+    table = soup.find("table", {"id": "main_table_countries_today"})
+    row_list = table.find_all("tr")
 
     for row in row_list:
-        data_soup = row.find_all('td')
+        data_soup = row.find_all("td")
 
-        if (len(data_soup) > 0):
+        if len(data_soup) > 0:
             if len(data_soup) == len(key_list):
                 continue
 
             data_text_list = list(map(process_data_item, data_soup))
             country_dict = dict(zip(key_list, data_text_list))
 
-            if type(country_dict['country']) != str:
+            if type(country_dict["country"]) != str:
                 continue
 
-            data_dict[country_dict['country'].lower()] = country_dict
+            data_dict[country_dict["country"].lower()] = country_dict
 
             print(f"{country_dict['country']} scraped!")
 
@@ -60,13 +62,15 @@ def get_table_data_from_soup(
 
 
 def scrape_worldometers_data() -> dict:
-    url = 'https://www.worldometers.info/coronavirus/#countries'
+    url = "https://www.worldometers.info/coronavirus/#countries"
     soup = get_soup_from_url(url=url)
     data_dict = get_table_data_from_soup(soup=soup)
 
     return data_dict
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     data_dict = scrape_worldometers_data()
     spreadsheet = gd.SpreadSheet()
-    spreadsheet.append_row(data_dict['spain']) 
+    spreadsheet.append_row(data_dict["spain"])
+
